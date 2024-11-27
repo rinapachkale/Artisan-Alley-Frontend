6d4473e2cc8e60fc50b3b7ddb6a86863283997df
Rina Pachkale <rinapachkale2@gmail.com>
2024-11-27 14:43:39 +0530

    Commit & push front page changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"; // Custom CSS
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and then save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="background-image"></div>{" "}
+      {/* Background image in App.css */}
+      <div className="content-container">
+        <div className="header-container">
+          <h1>Discover Handcrafted Treasures from Artisans Around the World</h1>
+          <h2>Artisan Alley</h2>
+          <p>Shop one-of-a-kind items made with passion and creativity.</p>
+          <button className="explore-button">Explore the Market</button>
+          <button className="explore-button">Admin Dashboard</button>
+          <button className="explore-button">Artisan Portal</button>
+        </div>
+      </div>
     </div>
   );
 }
